e009d8eaa6a16b781303417a6fcd1b794c9d08e0
joao oliveira <joaop.oliveira@ua.pt>
2023-11-09 23:46:45 +0000

    adiciona pagina de entrada

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -3,4 +3,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>{{$page_title}}</title>
+
+    <link rel="stylesheet" href="{{ asset('css/material-dashboard.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <script src="https://kit.fontawesome.com/db18a4139a.js" crossorigin="anonymous" defer></script>
 </head>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,24 @@
 @extends('layouts.base')
 
 @section('main')
-    <p>O conteúdo principal</p>
+<main>
+    <nav class="navbar navbar-main navbar-expand-lg px-3 shadow-none ">
+        <div class="container-fluid d-flex justify-content-end">
+            <button>Entrar</button>
+        </div>
+    </nav>
+    <div class="container-fluid d-flex align-items-center p-3 gap-3 bg-accent">
+        <i class="fa-solid fa-bars text-light"></i>
+        <div class="img-wrapper">
+            <img src="{{ asset('img/Logo.png') }}" alt="">
+        </div>
+    </div>
+
+    <div id="hero" class="m-3 p-4">
+        <h1 class="text-light">suporte à criação de horários da estga</h1>
+    </div>
+
+
+</div>
+</main>
 @endsection
