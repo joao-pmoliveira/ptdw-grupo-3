a93eec757e6241c63cd2f73dfc3e5707370c8a0a
joao oliveira <joaop.oliveira@ua.pt>
2023-11-10 00:23:29 +0000

    altera botao de login
    também remove classes do ficheiro css do template para que as alterações feitas no ficheiro principal sejam adotadas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,15 @@
 <main>
     <nav class="navbar navbar-main navbar-expand-lg px-3 shadow-none ">
         <div class="container-fluid d-flex justify-content-end">
-            <button>Entrar</button>
+            <button class="sm m-0 px-2 py-1 text-terciary bg-primary border-0
+                text-accent login-text" type="button">
+                    entrar
+                    <i class="fa-solid fa-user"></i>
+            </button>
         </div>
     </nav>
     <div class="container-fluid d-flex align-items-center p-3 gap-3 bg-accent">
-        <i class="fa-solid fa-bars text-light"></i>
+        <i class="fa-solid fa-bars text-light invisible"></i>
         <div class="img-wrapper">
             <img src="{{ asset('img/Logo.png') }}" alt="">
         </div>
