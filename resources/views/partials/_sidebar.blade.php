2106d8fa1e0f2b06281fbe573220d5262682095d
joao oliveira <joaop.oliveira@ua.pt>
2023-11-11 11:22:44 +0000

    corrige estilo da sidebar

--- resources/views/partials/_sidebar.blade.php
+++ resources/views/partials/_sidebar.blade.php
@@ -1,22 +1,22 @@
 <aside class="h-100">
     <ul class="h-100 m-0 p-0 d-flex flex-column flex-shrink-0 px-5 py-4 gap-4 bg-secondary">
         <li>
-            <a class="text-white" href="">Página Inicial</a>
+            <a class="active" href="">Página Inicial</a>
         </li>
         <li>
-            <a class="text-white" href="">Unidades Curriculares</a>
+            <a href="">Unidades Curriculares</a>
         </li>
         <li>
-            <a class="text-white" href="">Preencher Restrições</a>
+            <a  href="">Preencher Restrições</a>
         </li>
         <li>
-            <a class="text-white" href="">Gerir Processos</a>
+            <a  href="">Gerir Processos</a>
         </li>
         <li>
-            <a class="text-white" href="">Gerir Dados</a>
+            <a  href="">Gerir Dados</a>
         </li>
         <li>
-            <a class="text-white" href="">Ajuda</a>
+            <a  href="">Ajuda</a>
         </li>
     </ul>
 </aside>
