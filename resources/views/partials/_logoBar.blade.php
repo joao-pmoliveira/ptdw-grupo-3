e8610a350ddc96eb53ad601b520088dfe53f51b6
joao oliveira <joaop.oliveira@ua.pt>
2023-11-11 11:01:00 +0000

    corrige bug barra principal quando icon menu invis

--- resources/views/partials/_logoBar.blade.php
+++ resources/views/partials/_logoBar.blade.php
@@ -1,5 +1,5 @@
 <div class="container-fluid d-flex align-items-center px-5 py-3 gap-4 bg-accent">
-    <i class="fa-solid fa-bars text-light {{$sidebar ? '' : invisible}}"></i>
+    <i class="fa-solid fa-bars text-light {{$sidebar ? '' : 'invisible'}}"></i>
     <div class="img-wrapper">
         <img src="{{ asset('img/Logo.png') }}" alt="">
     </div>
