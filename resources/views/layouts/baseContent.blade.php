69aa6e5befe974697ca065ae84ca9598f7cb9d07
joao oliveira <joaop.oliveira@ua.pt>
2023-11-11 10:29:38 +0000

    adiciona barra de login

--- resources/views/layouts/baseContent.blade.php
+++ resources/views/layouts/baseContent.blade.php
@@ -2,7 +2,9 @@
 
 @section('main')
 
-<main class="vh-100">
+<main class="vh-100 d-flex flex-column">
+
+    @include('partials._topBar')
 
     @include('partials._logoBar', ['sidebar' => true])
     <div class="d-flex h-100">
---
+++ resources/views/partials/_topBar.blade.php
@@ -0,0 +1,6 @@
+<ul class="d-flex justify-content-end px-5 py-2 gap-2 login-txt text-accent">
+    <li>User</li>
+    <li>|</li>
+    <li>sair</li>
+    <li><i class="fa-solid fa-user"></i></li>
+</ul>
