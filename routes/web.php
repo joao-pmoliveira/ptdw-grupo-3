e1e67076daf4eb6f7ce6f12c392f2062b25c4ef1
joao oliveira <joaop.oliveira@ua.pt>
2023-11-12 15:04:32 +0000

    renomea view 'gerirDados' para 'manageData'

--- routes/web.php
+++ routes/web.php
@@ -71,5 +71,5 @@ Route::get('/uc/{id}', function () {
 
 //Página de Gerir Dados (Admin)
 Route::get('/manage', function () {
-    return view('gerirDados', ['page_title' => 'Gerir Dados']);
+    return view('manageData', ['page_title' => 'Gerir Dados']);
 });
