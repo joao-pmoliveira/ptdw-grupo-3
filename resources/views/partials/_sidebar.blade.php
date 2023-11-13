06ed502bdcc4a6cb53adb1ea8f860e79d8eea64a
joao oliveira <joaop.oliveira@ua.pt>
2023-11-13 10:16:57 +0000

    atualiza links na sidebar

--- resources/views/partials/_sidebar.blade.php
+++ resources/views/partials/_sidebar.blade.php
@@ -1,22 +1,22 @@
 <aside class="dvh-100">
     <ul class="h-100 m-0 p-0 d-flex flex-column flex-shrink-0 px-5 py-4 gap-4 bg-secondary">
         <li>
-            <a class="active" href="">Página Inicial</a>
+            <a class="{{Request::is('inicio') ? 'active' : ''}}" href="{{url('/inicio')}}">Página Inicial</a>
         </li>
         <li>
-            <a href="">Unidades Curriculares</a>
+            <a class="{{Request::is('ucs') ? 'active' : ''}}" href="{{url('/ucs')}}">Unidades Curriculares</a>
         </li>
         <li>
-            <a  href="">Preencher Restrições</a>
+            <a class="{{Request::is('restrições') ? 'active' : ''}}" href="{{url('/restrições')}}">Preencher Restrições</a>
         </li>
         <li>
-            <a  href="">Gerir Processos</a>
+            <a class="{{Request::is('processos') ? 'active' : ''}}" href="{{url('/processos')}}">Gerir Processos</a>
         </li>
         <li>
-            <a  href="">Gerir Dados</a>
+            <a class="{{Request::is('gerir') ? 'active' : ''}}" href="{{url('/gerir')}}">Gerir Dados</a>
         </li>
         <li>
-            <a  href="">Ajuda</a>
+            <a class="" href="">Ajuda</a>
         </li>
     </ul>
 </aside>
