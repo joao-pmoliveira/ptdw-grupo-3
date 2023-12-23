f2cf73a3e703e00ab37ab53059ee9d0fc524710a
joao oliveira <joaop.oliveira@ua.pt>
2023-12-23 23:18:46 +0000

    simplifica rota em breadcrumbs

--- resources/views/partials/_breadcrumbs.blade.php
+++ resources/views/partials/_breadcrumbs.blade.php
@@ -1,7 +1,7 @@
 <ul class="breadcrumb my-4 bg-primary text-primary">
     @foreach ($crumbs as $index => $crumb)
         <li class="breadcrumb-item breadcrumb-txt  @if($loop->last) active @endif" >
-            <a href="{{url($crumb[1])}}">{{$crumb[0]}}</a>
+            <a href="{{ $crumb[1] }}">{{ $crumb[0] }}</a>
         </li>
     @endforeach
 </ul>
