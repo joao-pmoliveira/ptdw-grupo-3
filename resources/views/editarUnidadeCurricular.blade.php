febfcd988e0fb69c9914e9fc34ef1eb3b67ea590
joao oliveira <joaop.oliveira@ua.pt>
2024-01-03 02:40:47 +0000

    add consistencia entre views de dados uc
    alterações estruturais e visuais para que a pagina de consulta de dados
    de uma UC e a pagina para edição de dados de uma UC seja mais
    semelhantes

--- resources/views/editarUnidadeCurricular.blade.php
+++ resources/views/editarUnidadeCurricular.blade.php
@@ -10,70 +10,77 @@
         ]
     ])
 
-    @include('partials._pageTitle', ['title' => $uc->nome . ' - ' . $uc->id])
+    @include('partials._pageTitle', ['title' => $uc->codigo . ' - ' . $uc->nome])
 
     <section class="mt-3">
         <form id="edit-uc-form" action="" method="post" class="title-separator">
             <div class="mb-3">
-                <input type="text" name="" id="" value="{{$uc->codigo}}" placeholder="Cód" required>
-                <input type="text" name="" id="" value="{{$uc->nome}}" placeholder="Nome" required>
             </div>
             
-            <fieldset>
-                <div class="mb-2">
-                    <label for="uc-area">Área Científica</label>
-                    <select name="uc_area" id="uc_area" required>
-                        <option value="" selected>Selecione</option>
-                        @foreach($acns as $acn)
-                        <option value="{{$acn->sigla}}">{{$acn->sigla}}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="mb-2">
-                    <label for="uc-ects">ECTSs</label>
-                    <input type="number" name="uc_ects" id="uc-ects" value="{{$uc->ects}}" placeholder="Número de créditos" required>
-                </div>
-                <div class="mb-2">
-                    <label for="uc-horas">Horas semanais</label>
-                    <input type="number" name="uc_horas" id="uc-horas" value="{{$uc->horas_semanais}}" placeholder="Número de horas semais" required>
-                </div>
-                <div class="mb-2">
-                    <label for="uc-main-teacher">Docente Responsável</label>
-                    <select name="uc_main_teacher" id="uc-main-teacher" required>
+            <div class="d-flex align-items-center border border-dark p-2 mb-2">
+                <label class="col-md-2 p-3" for="uc-codigo">Código</label>
+                <input class="col-md-2 p-1" type="text" name="uc-codigo" id="uc-codigo-input" value="{{$uc->codigo}}" placeholder="Cód" required>
+            </div>
+            
+            <div class="d-flex align-items-center border border-dark p-2 mb-2">
+                <label class="col-md-2 p-3" for="uc-nome">Nome</label>
+                <input class="col-md-6 p-1" type="text" name="uc-nome" id="uc-nome-input" value="{{$uc->nome}}" placeholder="Nome" required>
+            </div>
+            <div class="d-flex align-items-center border border-dark p-2 mb-2">
+                <label class="col-md-2 p-3" for="uc-acn">Área Científica</label>
+                <select class="col-md-2 p-1" name="uc-acn" id="uc-acn-select" required>
                     <option value="" selected>Selecione</option>
+                    @foreach ($acns as $acn)
+                        <option value="{{$acn->sigla}}">{{$acn->sigla}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="d-flex align-items-center border border-dark p-2 mb-2">
+                <label class="col-md-2 p-3" for="uc-ects">ECTS</label>
+                <input class="col-md-2 p-1" type="number" name="uc-ects" id="uc-ects-input" required>
+            </div>
+            <div class="d-flex align-items-center border border-dark p-2 mb-2">
+                <label class="col-md-2 p-3" for="uc-horas">Horas Semanais</label>
+                <input class="col-md-2 p-1" type="number" name="uc-horas" id="uc-horas-input" required>
+            </div>
+            <div class="d-flex align-items-center border border-dark p-2 mb-2">
+                <label class="col-md-2 p-3" for="uc-main-teacher">Docente Responsável</label>
+                <select class="col-md-2 p-1" name="uc-main-teacher" id="uc-main-teacher-select" required>
+                    <option value="">Selecione</option>
+                    @foreach ($docentes as $docente)
+                        <option value="{{$docente->id}}">{{$docente->numero_funcionario .' - '. $docente->user->nome}}</option>   
+                    @endforeach
+                </select>
+            </div>
+            <div class="d-flex align-items-center border border-dark p-2 mb-2">
+                <label class="col-md-2 p-3" for="uc-teachers">Restantes Docentes</label>
+                <div class="d-flex flex-column gap-2 col-md-4 p-1">
+                    <select name="uc-teachers" id="uc-teachers-select-1">
+                        <option value="">Selecione</option>
                         @foreach($docentes as $docente)
-                        <option value="{{$docente->numero_funcionario}}">{{$docente->numero_funcionario}} {{$docente->nome}}</option>
+                            <option value="{{$docente->id}}">{{$docente->numero_funcionario .' - '. $docente->user->nome}}</option>
+                        @endforeach
+                    </select>
+                    <select name="uc-teachers" id="uc-teachers-select-2">
+                        <option value="">Selecione</option>
+                        @foreach($docentes as $docente)
+                            <option value="{{$docente->id}}">{{$docente->numero_funcionario .' - '. $docente->user->nome}}</option>
+                        @endforeach
+                    </select>
+                    <select name="uc-teachers" id="uc-teachers-select-3">
+                        <option value="">Selecione</option>
+                        @foreach($docentes as $docente)
+                            <option value="{{$docente->id}}">{{$docente->numero_funcionario .' - '. $docente->user->nome}}</option>
+                        @endforeach
+                    </select>
+                    <select name="uc-teachers" id="uc-teachers-select-4">
+                        <option value="">Selecione</option>
+                        @foreach($docentes as $docente)
+                            <option value="{{$docente->id}}">{{$docente->numero_funcionario .' - '. $docente->user->nome}}</option>
                         @endforeach
                     </select>
                 </div>
-                <div class="mb-2">
-                    
-                    <label for="uc-teachers">Restantes Docentes</label>
-                    <div class="d-flex flex-column gap-2">
-                        <select name="uc_teachers" id="uc-teachers">
-                            <option value="" selected>Selecione</option>
-                            @foreach($docentes as $docente)
-                            <option value="{{$docente->numero_funcionario}}">{{$docente->numero_funcionario}} {{$docente->nome}}</option>
-                            @endforeach
-                        </select>
-                        <select name="uc_teachers" id="uc-teachers">
-                            <option value="" selected>Selecione</option>
-                            @foreach($docentes as $docente)
-                            <option value="{{$docente->numero_funcionario}}">{{$docente->numero_funcionario}} {{$docente->nome}}</option>
-                            @endforeach
-                        </select>
-                        <select name="uc_teachers" id="uc-teachers">
-                            <option value="" selected>Selecione</option>
-                            @foreach($docentes as $docente)
-                            <option value="{{$docente->numero_funcionario}}">{{$docente->numero_funcionario}} {{$docente->nome}}</option>
-                            @endforeach
-                        </select>
-                        <button class="btn">
-                            Adicionar Docente
-                        </button>
-                    </div>
-                </div>
-            </fieldset>
+            </div>
 
             <div class="d-flex gap-3 mt-3 mb-5" id="form-btns">
                 <input class="btn" type="button" value="Submeter">
--- resources/views/unidadeCurricular.blade.php
+++ resources/views/unidadeCurricular.blade.php
@@ -12,73 +12,60 @@
     ])
 
     @include('partials._pageTitle', ['title' => $uc->codigo . ' - ' . $uc->nome])
-    <!--  trocar Calculo I pelo nome da cadeira em, questao  - "$nomeUC"-->
-
 
     <section class="mt-3">
         <div class="mb-3 title-separator p-2">
         </div>
 
         <div class="mt-3">
-
-            <div class="d-flex aling-items-center border border-dark p-2 mb-2 ">
-                <div class="col-4 col-md-2 p-3">Área Científica</div>
-                <div class="col-8 col-md-10 p-3"><p>{{ $uc->acn->nome }}</p></div>
+            <div class="d-flex align-items-center border border-dark p-2 mb-2 ">
+                <div class="col-md-2 p-3">Área Científica</div>
+                <div class="col-md-10 p-3"><p>{{ $uc->acn->nome }}</p></div>
             </div>
 
-            <div class="d-flex aling-items-center border border-dark p-2 mb-2 ">
-                <div class="col-4 col-md-2 p-3">ECTS's</div>
-                <div class="col-8 col-md-10 p-3"><p>{{ $uc->ects }}</p></div>
+            <div class="d-flex align-items-center border border-dark p-2 mb-2 ">
+                <div class="col-md-2 p-3">ECTS</div>
+                <div class="col-md-10 p-3"><p>{{ $uc->ects }}</p></div>
             </div>
 
-            <div class="d-flex aling-items-center border border-dark p-2 mb-2 ">
-                <div class="col-4 col-md-2 p-3">Horas Semanais</div>
-                <div class="col-8 col-md-10 p-3"><p>{{ $uc->horas_semanais}} horas</p></div>
+            <div class="d-flex align-items-center border border-dark p-2 mb-2 ">
+                <div class="col-md-2 p-3">Horas Semanais</div>
+                <div class="col-md-10 p-3"><p>{{ $uc->horas_semanais}} horas</p></div>
             </div>
 
-            <div class="d-flex aling-items-center border border-dark p-2 mb-2 ">
-                <div class="col-4 col-md-2 p-3">Docente Responsável</div>
-                <div class="col-8 col-md-10 p-3"><p>{{ $uc->docenteResponsavel->numero_funcionario }} - {{ $uc->docenteResponsavel->nome }}</p></div>
+            <div class="d-flex align-items-center border border-dark p-2 mb-2 ">
+                <div class="col-md-2 p-3">Docente Responsável</div>
+                <div class="col-md-10 p-3">
+                    <p>{{$uc->docenteResponsavel->numero_funcionario .' - '. $uc->docenteResponsavel->user->nome}}</p>
+                </div>
             </div>
 
-            <div class="d-flex aling-items-center border border-dark p-2 mb-2 ">
-                <div class="col-4 col-md-2 p-3">Restantes Docentes</div>
-                <div class="col-8 col-md-10 p-3">
+            <div class="d-flex align-items-center border border-dark p-2 mb-2 ">
+                <div class="col-md-2 p-3">Restantes Docentes</div>
+                <div class="col-md-10 p-3">
                     @foreach ( $uc->docentes as $docente)
-                        <p class="mb-2">{{ $docente->numero_funcionario }} - {{ $docente->nome }}</p>
+                        <p>{{$docente->numero_funcionario .' - '. $docente->user->nome}}</p>
                     @endforeach
                 </div>
             </div>
 
-            <div class="d-flex aling-items-center border border-dark p-2 mb-2 ">
-                <div class="col-4 col-md-2 p-3">Restrições de Unidade Curricular </div>
-                <div class="col-8 col-md-10 p-3">
+            <div class="d-flex align-items-center border border-dark p-2 mb-2 ">
+                <div class="col-md-2 p-3">Restrições de Unidade Curricular </div>
+                <div class="col-md-10 p-3">
                     <div class="mb-2">
-                        Tipo de sala de aula:
-                        @if ($uc->laboratorio === 1)
-                            <p>Laboratório de Informática</p>
-                        @elseif ($uc->laboratorio === 0)
-                            <p>Sala Comum</p>
-                        @endif
-                    <div>
+                        <h3 class="font-weight-normal">Tipo de Sala de aula:</h3>
+                        <p>{{$uc->laboratorio ? 'Laboratório de Informática' : 'Sala Comum'}}</p>
+                    </div>
 
                     <div class="mb-2">
-                        Software necessário:
-                        @if ($uc->software)
-                            <p>{{ $uc->software }}</p>
-                        @else
-                            <p>Não existe software necessário.</p>
-                        @endif
-                    <div>
+                        <h3 class="font-weight-normal">Software necessário</h3>
+                        <p>{{$uc->software ? $uc->software : 'Não especificado.'}}</p>
+                    </div>
 
                     <div class="mb-2">
-                        Tipo de sala para avaliação:
-                        @if ($uc->sala_avaliacao === 1)
-                            <p>Laboratório de Informática</p>
-                        @elseif ($uc->sala_avaliacao === 0)
-                            <p>Sala Comum</p>
-                        @endif
-                    <div>
+                        <h3 class="font-weight-normal">Tipo de Sala para Avaliação:</h3>
+                        <p>{{$uc->sala_avaliacao ? 'Laboratório de Informática' : 'Sala Comum'}}</p>
+                    </div>
                 </div>
             </div>
         </div>
