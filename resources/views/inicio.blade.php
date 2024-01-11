e6bc0485c262a839a11ef0329a1f4183fb08fd81
joao oliveira <joaop.oliveira@ua.pt>
2024-01-11 22:34:18 +0000

    reorganiza pagina inicial

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -9,39 +9,50 @@
 
     @include('partials._pageTitle', ['title' => 'Suporte à Criação de Horários'])
 
-    <section class="mt-5 d-flex flex-wrap gap-4">
-        @include('partials._card', [
-                'title' => 'Unidades Curriculares',
-                'body' => ['Consulta da lista de Unidades Curriculares'],
-                'button' => 'Consultar',
-                'url' => route('ucs.view')
-            ])
-        @include('partials._card', [
-                'title' => 'Preencher Restrições',
-                'body' => [
-                        'Prenchimento de impedimentos de horários e restrições de sala',
-                    ],
-                'button' => 'Preencher',
-                'url' => route('restricoes.view')
-            ])
-        @include('partials._card', [
-                'title' => 'Recolha de Restrições',
-                'body' => [
-                        'Criação de um novo processo e verificação do mesmo',
-                        'Descarregar dados de restrições'
-                    ], 
-                'button' => 'Gerir',
-                'url' => route('restricoes.recolha.view')
-            ])
-        @include('partials._card', [
-                'title' => 'Gerir Dados',
-                'body' => [
-                        'Adicionar, editar ou remover Unidades Curriculares e docentes;',
-                        'Importar dados serviço-docente;'
-                    ],
-                'button' => 'Gerir',
-                'url' => route('admin.gerir.view')
-            ])
+    <section class="mt-3">
+        <h2 class="m-2 text-terciary">Docente</h2>
+        <div class="d-flex flex-wrap gap-4">
+            @include('partials._card', [
+                    'title' => 'Unidades Curriculares',
+                    'body' => ['Consulta da lista de Unidades Curriculares'],
+                    'button' => 'Consultar',
+                    'url' => route('ucs.view')
+                ])
+            @include('partials._card', [
+                    'title' => 'Preencher Restrições',
+                    'body' => [
+                            'Prenchimento de impedimentos de horários e restrições de sala',
+                        ],
+                    'button' => 'Preencher',
+                    'url' => route('restricoes.view')
+                ])
+        </div>
+    </section>
+    <hr>
+    <section class="mt-3">
+        <h2 class="m-2 text-terciary">Admin</h2>
+        <div class="d-flex flex-wrap gap-4">
+            @include('partials._card', [
+                    'title' => 'Recolha de Restrições',
+                    'body' => [
+                            'Criação de um novo processo e verificação do mesmo',
+                            'Descarregar dados de restrições'
+                        ], 
+                    'button' => 'Gerir',
+                    'url' => route('restricoes.recolha.view')
+                ])
+            @include('partials._card', [
+                    'title' => 'Gerir Dados',
+                    'body' => [
+                            'Adicionar, editar ou remover Unidades Curriculares e docentes;',
+                            'Importar dados serviço-docente;'
+                        ],
+                    'button' => 'Gerir',
+                    'url' => route('admin.gerir.view')
+                ])
+        </div>
+    </section>
+    <section class="mt-3" hidden>
         @include('partials._card', [
                 'title' => 'Ajuda',
                 'body' => [
@@ -50,8 +61,11 @@
                 'button' => 'Consultar',
                 'url' => route('inicio.view')
             ])
-
     </section>
+        
+        
+        
+
 </main>
 
 @endsection
