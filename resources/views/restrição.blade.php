925823774a87c13cb04df98e3f4112f4beda7a81
Gonçalo Menezes <goncalomenezes@ua.pt>
2024-01-17 17:30:52 +0000

    botão para preencher com restricao anterior

--- resources/views/restrição.blade.php
+++ resources/views/restrição.blade.php
@@ -21,21 +21,29 @@
             @csrf
             @method('PUT')
             <div class="title-separator d-flex flex-column gap-3 py-4">
+                @if($editavel && $ultimaRestricao_uc)
+                    <div class="form-check p-0">
+                        <label class="form-check-label" for="ultimaRestricao">
+                            Preencher com restrição anterior
+                        </label>
+                        <input class="form-check-input" type="checkbox" name="ultimaRestricao" id="ultimaRestricao">
+                    </div>
+                @endif
                 <fieldset>
                     <legend>Requisão de Laboratórios</legend>
                     <div class="col-md-3 d-flex justify-content-between">
                         <label for="aula-laboratorio-input">Sala de Aula</label>
-                        <input type="checkbox" name="sala_laboratorio" id="aula-laboratorio-input"
+                        <input type="checkbox" data-history="{{$ultimaRestricao_uc->sala_laboratorio}}" name="sala_laboratorio" id="aula-laboratorio-input"
                             @checked($uc->sala_laboratorio) @disabled(!$editavel)>
                     </div>
                     <div class="col-md-3 d-flex justify-content-between">
                         <label for="exame-final-laboratorio">Exame Época Normal</label>
-                        <input type="checkbox" name="exame_final_laboratorio" id="exame-final-laboratorio"
+                        <input type="checkbox" data-history="{{$ultimaRestricao_uc->exame_final_laboratorio}}" name="exame_final_laboratorio" id="exame-final-laboratorio"
                             @checked($uc->exame_final_laboratorio) @disabled(!$editavel)>
                     </div>
                     <div class="col-md-3 d-flex justify-content-between">
                         <label for="exame-recurso-laboratorio">Exame Época Recurso</label>
-                        <input type="checkbox" name="exame_recurso_laboratorio" id="exame-recurso-laboratorio"
+                        <input type="checkbox" data-history="{{$ultimaRestricao_uc->exame_recurso_laboratorio}}"  name="exame_recurso_laboratorio" id="exame-recurso-laboratorio"
                             @checked($uc->exame_recurso_laboratorio) @disabled(!$editavel)>
                     </div>
                 </fieldset>
@@ -43,20 +51,25 @@
                 <fieldset>
                     <legend>Observações sobre requisão de salas/laboratórios</legend>
                     <label class="d-none" for="observacoes-input">Observações</label>
-                    <textarea class="px-2 py-1" name="observacoes" id="observacoes-input" cols="70" rows="5"
+                    <textarea class="px-2 py-1" data-history="{{$ultimaRestricao_uc->observacoes_laboratorios}}" name="observacoes" id="observacoes-input" cols="70" rows="5"
                         @disabled(!$editavel)>{{$uc->observacoes_laboratorios}}</textarea>
                 </fieldset>
 
                 <fieldset>
                     <legend>Software necessário</legend>
                     <label class="d-block" for="software-necessario-input">Identifique nome, fabricante, versão e sistem operativo</label>
-                    <textarea class="px-2 py-1" name="software" id="software-necessario-input" cols="90" rows="8"
+                    <textarea class="px-2 py-1" data-history="{{$ultimaRestricao_uc->software}}" name="software" id="software-necessario-input" cols="90" rows="8"
                         @disabled(!$editavel)>{{$uc->software}}</textarea>
                 </fieldset>
             </div>
             <div class="d-flex gap-3 mb-5" id="form-btns">
-                <input class="btn" type="submit" value="Submeter" @disabled(!$editavel)>
-                <a class="btn cancelar" href="{{route('restricoes.view')}}" value="Cancelar">Cancelar</a>
+                @if($editavel)
+                    <input class="btn" type="submit" value="Submeter" @disabled(!$editavel)>
+                    <a class="btn cancelar" href="{{route('restricoes.view')}}" value="Cancelar">Cancelar</a>
+                    <input class="btn" type="reset" value="Reset">
+                @elseif(!$editavel)
+                    <a class="btn cancelar" href="{{route('restricoes.view')}}" value="Cancelar">Cancelar</a>
+                @endif
             </div>
         </form>
     </section>
