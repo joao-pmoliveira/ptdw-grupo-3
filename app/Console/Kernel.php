3a02da51c9015ae3ced04bd62bf6cdbcdedd7f56
Jorge <miguelmvieira@ua.pt>
2024-01-18 11:46:26 +0000

    teste cron mail

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('emails:send')->cron('50 11 18 1 *');
     }
 
     /**
