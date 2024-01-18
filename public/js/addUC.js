1322a59239afb3ee0c7017dfe7acc06c12cf0b96
joao oliveira <joaop.oliveira@ua.pt>
2024-01-18 14:14:03 +0000

    remove comentario em addUC

--- public/js/addUC.js
+++ public/js/addUC.js
@@ -1,34 +1,5 @@
 'use strict'
 
-// const addUCForm = document.querySelector('#add-uc-form');
-// addUCForm.addEventListener('submit', async (e) => {
-//     e.preventDefault();
-
-//     const formData = new FormData(addUCForm);
-
-//     try {
-//         const res = await fetch(addUCForm.action, {
-//             method: 'POST',
-//             body: formData,
-//         });
-
-//         if (!res.ok) {
-//             throw new Error(`HTTP Error! Status: ${res.status}, Message: ${res.message}`);
-//         }
-
-//         const data = await res.json();
-
-//         if (data.redirect) {
-//             window.location.href = data.redirect;
-//         }
-
-//     } catch (error) {
-//         console.error(`Error: ${error.message}`);
-//         console.error(`Error stack: ${error.stack}`);
-//     }
-
-// })
-
 //todo @joao: atualizar para que funcione da mesma maneira do que 'editarUC'
 const docentesSelect = Array.from(document.querySelectorAll('select:not(:is([id="uc-acn-select"]))'));
 docentesSelect.forEach((select) => {
