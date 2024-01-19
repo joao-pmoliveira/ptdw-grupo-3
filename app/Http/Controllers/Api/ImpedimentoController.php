12340c1b6f9f2464577351c1cd1abfdb78dd2113
Jorge <miguelmvieira@ua.pt>
2024-01-19 17:32:00 +0000

    debug mail abertura v6

--- app/Http/Controllers/Api/ImpedimentoController.php
+++ app/Http/Controllers/Api/ImpedimentoController.php
@@ -199,8 +199,8 @@ class ImpedimentoController extends Controller
 
             // todo @joao: passar para dentro do ciclo anterior
             foreach ($docentes as $docente) {
-                $filteredUcsResp = $docente->ucsResponsavel()->where('periodo', $periodo)->get();
-                $filteredUcs = $docente->unidadesCurriculares()->where('periodo', $periodo)->get();
+                $filteredUcsResp = $docente->ucsResponsavel()->where('periodo_id', $periodo->id)->get();
+                $filteredUcs = $docente->unidadesCurriculares()->where('periodo_id', $periodo->id)->get();
                 if(empty($docente->user->email)){
                     continue;
                 }
