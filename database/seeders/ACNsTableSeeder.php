603e65c5712b56a1c725f7448d29c98d01cec7f2
joao <joaop.oliveira@ua.pt>
2024-01-26 15:03:52 +0000

    atualiza lista acns para lista real da ESTGA
    lista de ACNs no seeder reflete a lista de ACNs usada pela ESTGA nas
    unidades curriculares

--- database/seeders/ACNsTableSeeder.php
+++ database/seeders/ACNsTableSeeder.php
@@ -9,15 +9,21 @@ class ACNsTableSeeder extends Seeder
 {
     public function run(): void
     {
-        //Lista exemplo de áreas científicas
         $acns = [
-            ['nome' => 'Informática', 'sigla' => 'I'],
-            ['nome' => 'Física', 'sigla' => 'F'],
-            ['nome' => 'Matemática', 'sigla' => 'M'],
-            ['nome' => 'Química', 'sigla' => 'Q'],
-            ['nome' => 'Biologia', 'sigla' => 'B'],
-            ['nome' => 'Gestão', 'sigla' => 'GES'],
             ['nome' => 'Línguas', 'sigla' => 'L'],
+            ['nome' => 'Informática', 'sigla' => 'I'],
+            ['nome' => 'Matemática', 'sigla' => 'M'],
+            ['nome' => 'Engenharia Geográfica', 'sigla' => 'EGG'],
+            ['nome' => 'Gestão', 'sigla' => 'GES'],
+            ['nome' => 'Ciências Jurídicas', 'sigla' => 'CJ'],
+            ['nome' => 'Secretariado e Comunicação Empresarial', 'sigla' => 'SCE'],
+            ['nome' => 'Contabilidade', 'sigla' => 'C'],
+            ['nome' => 'Ciências Sociais', 'sigla' => 'CS'],
+            ['nome' => 'Economia', 'sigla' => 'E'],
+            ['nome' => 'Ciências da Engenharia', 'sigla' => 'CENG'],
+            ['nome' => 'Engenharia Mecânica', 'sigla' => 'EMEC'],
+            ['nome' => 'Eletrotecnia', 'sigla' => 'ELE'],
+            ['nome' => 'Física', 'sigla' => 'F'],
         ];
 
         foreach ($acns as $acn) {
